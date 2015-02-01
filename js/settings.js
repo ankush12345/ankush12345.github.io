c9dbf3a0fa8bc1c71b7dd2f1ffb477ce85f8a97f
ankush12345 <shanky.rich@gmail.com>
2015-02-01 16:34:04 +0530

    gkdfj
    fgmj

--- js/settings.js
+++ js/settings.js
@@ -48,7 +48,7 @@
 
 		var weddingDate = new Date();
 	    //var weddingDate = new Date(weddingDate.getFullYear() + 1, 1 - 1, 1);
-		var weddingDate = new Date("2015-1-27");
+		var weddingDate = new Date("2015-2-12");
 		$(".countdown").countdown({
 			until: weddingDate,
 			format: 'ODHMS'
